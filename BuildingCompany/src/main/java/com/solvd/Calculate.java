cded9498bcaa22a8ad0ae1a65d10ef15175d8d70
laurot <laurot275@gmail.com>
2022-03-23 11:30:19 -0300

    eh yeah

--- BuildingCompany/src/main/java/com/solvd/Calculate.java
+++ BuildingCompany/src/main/java/com/solvd/Calculate.java
@@ -1,4 +1,5 @@
-package main.java.com.solvd;
+package com.solvd;
+
 import main.java.com.solvd.buildings.*;
 import main.java.com.solvd.interfaces.ICalc;
 import main.java.com.solvd.interfaces.ICalculate;
@@ -11,62 +12,65 @@ public class Calculate implements ICalculate{
     public void calculate(int buildType,int weather, int serviceType, int floors, float sqMeters, ITax country) {
         float time = 0;
         float price = 0;
-        switch(buildType){
-            case 2:
+        switch (buildType) {
+            case 2 -> {
                 //Residential
                 ICalc resi = new Residential(floors, sqMeters);
                 time = resi.calcTime(time);
                 price = resi.calcPrice(price);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 //Industrial
                 ICalc indu = new Industrial(floors, sqMeters);
                 time = indu.calcTime(time);
                 price = indu.calcPrice(price);
-                break;
-            default:
-                //Comercial
-                ICalc comer = new Comercial(floors,sqMeters);
+            }
+            default -> {
+                //Commercial
+                ICalc comer = new Comercial(floors, sqMeters);
                 time = comer.calcTime(time);
                 price = comer.calcPrice(price);
+            }
         }
-        switch(weather){
-            case 2:
+        switch (weather) {
+            case 2 -> {
                 //Dry Season
                 ICalc dryS = new DrySeason();
                 time = dryS.calcTime(time);
                 price = dryS.calcPrice(price);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 //Rainy Season
                 ICalc wetS = new RainSeason();
                 time = wetS.calcTime(time);
                 price = wetS.calcPrice(price);
-                break;
-            default:
+            }
+            default -> {
                 //Normal Season
                 ICalc normS = new NormalSeason();
                 time = normS.calcTime(time);
                 price = normS.calcPrice(price);
+            }
         }
-        switch(serviceType){
-            case 2:
+        switch (serviceType) {
+            case 2 -> {
                 //Fast Service
                 ICalc fastS = new FastService();
                 time = fastS.calcTime(time);
                 price = fastS.calcPrice(price);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 //Luxurious Service
                 ICalc luxS = new LuxuriousService();
                 time = luxS.calcTime(time);
                 price = luxS.calcPrice(price);
-                break;
-            default:
+            }
+            default -> {
                 //Normal Service
                 ICalc normS = new NormalService();
                 time = normS.calcTime(time);
                 price = normS.calcPrice(price);
+            }
         }
         System.out.println("----------------------------------------------");
         System.out.println("The price of building is: $" + country.tax(price));
